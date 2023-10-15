088b6715d986b828aa27c647a4c246a256272ed0
Julian <mrjrhirst@gmail.com>
2023-10-15 17:16:34 +0100

    ternaries complete

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,4 @@
 // Instructions can be found in ternaries.md
 
-export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
-}
+export const sayHello = (name) => name ? `Hello, ${ name }!` : 'Hello you!';
+
